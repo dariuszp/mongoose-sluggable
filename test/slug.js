1eb1ed2f5efb2ca6c332bec05513fe529ab74704
dariuszp <poltorak.dariusz@gmail.com>
2015-09-03 16:30:15 +0200

    travis

---
+++ test/slug.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var should = require('should');
+
+describe('slug', function () {
+    describe('dummy test', function () {
+        it ('dummy description for passed test', function () {
+            should(true).be.ok;
+        });
+    });
+});
